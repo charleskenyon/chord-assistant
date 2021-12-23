2a81650eadbe391bc2913d2cb9d154d55ced6c5d
Rory Kenyon <rory.kenyon01@gmail.com>
2021-12-23 12:28:38 +0000

    amend includes polyfill type signature

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,5 +1,8 @@
 if (!Array.prototype.includes) {
-  Array.prototype.includes = function (search: string | number): boolean {
+  Array.prototype.includes = function <T, U extends T>(
+    this: T[],
+    search: U
+  ): boolean {
     return !!~this.indexOf(search);
   };
 }
